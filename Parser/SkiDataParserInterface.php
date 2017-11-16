3c5e585725a0b6fbacda0f9b3185f449113bdda0
webeweb <support@webeweb.fr>
2017-11-16 17:24:12 +0100

    Add SkiData perser interface

---
+++ Parser/SkiDataParserInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the skidata-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SkiData\Parser;
+
+/**
+ * SkiData parser interface.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SkiData\Parser
+ */
+interface SkiDataParserInterface {
+
+	/**
+	 * Date format.
+	 */
+	const DATE_FORMAT = 'Ymd';
+
+	/**
+	 * Date/time format.
+	 */
+	const DATETIME_FORMAT = 'Ymd His';
+
+}
