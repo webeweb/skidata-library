e8a5c0e4c3eed90cbdcbc69f600d8a9ff3b5a316
webeweb <support@webeweb.fr>
2017-11-16 17:20:12 +0100

    Add abstract SkiData exception

---
+++ Exception/AbstractSkiDataException.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the skidata-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SkiData\Exception;
+
+use SebastianBergmann\RecursionContext\Exception;
+use WBW\Library\Core\Exception\AbstractWBWException;
+
+/**
+ * Abstract SkiData exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SkiData\Exception
+ * @abstract
+ */
+abstract class AbstractSkiDataException extends AbstractWBWException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $message The message.
+	 * @param Exception $previous The previous exception.
+	 */
+	public function __construct($message, Exception $previous = null) {
+		parent::__construct($message, $previous);
+	}
+
+}
