22dfde832082ccef2a5224288dadc7032036935a
webeweb <support@webeweb.fr>
2017-11-16 17:24:58 +0100

    Add SkiData start record format parser

---
+++ Parser/SkiDataStartRecordFormatParser.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the skidata-library package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SkiData\Parser;
+
+use WBW\Library\SkiData\Entity\SkiDataCustomerEntity;
+use WBW\Library\SkiData\Entity\SkiDataStartRecordFormatEntity;
+
+/**
+ * SkiData start record format parser.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SkiData\Parser
+ * @final
+ */
+final class SkiDataStartRecordFormatParser extends AbstractSkiDataParser {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Parse a SkiData start record format entity.
+	 *
+	 * @param SkiDataCustomerEntity $entity The SkiData start record format entity.
+	 * @return string Returns the parsed SkiData start record format entity.
+	 */
+	public function parseEntity(SkiDataStartRecordFormatEntity $entity) {
+
+		// Initialise the output.
+		$output = [];
+
+		$output[]	 = $this->encodeInteger($entity->getVersionRecordStructure(), 6);
+		$output[]	 = $this->encodeInteger($entity->getFacilityNumber(), 7);
+		$output[]	 = $this->encodeDate($entity->getDateFile());
+		$output[]	 = $this->encodeInteger($entity->getNumberRecords(), 5);
+		$output[]	 = $this->encodeString($entity->getCurrency(), 6);
+
+		// Return the output.
+		return implode(";", $output);
+	}
+
+	/**
+	 * Parse a line.
+	 *
+	 * @param string $line The line.
+	 * @return SkiDataStartRecordFormatEntity Returns a SkiData start record format entity.
+	 */
+	public function parseLine($line) {
+
+		// Split the line.
+		$data	 = explode(";", $line);
+		$i		 = 0;
+
+		// Initialize the entity.
+		$entity = new SkiDataStartRecordFormatEntity();
+
+		$entity->setVersionRecordStructure($this->decodeInteger($data[$i++]));
+		$entity->setFacilityNumber($this->decodeInteger($data[$i++]));
+		$entity->setDateFile($this->decodeDate($data[$i++]));
+		$entity->setNumberRecords($this->decodeInteger($data[$i++]));
+		$entity->setCurrency($this->decodeString($data[$i++]));
+
+		// Return the entity.
+		return $entity;
+	}
+
+}
---
+++ Tests/Parser/SkiDataStartRecordFormatParserTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * Disclaimer: This source code is protected by copyright law and by
+ * international conventions.
+ *
+ * Any reproduction or partial or total distribution of the source code, by any
+ * means whatsoever, is strictly forbidden.
+ *
+ * Anyone not complying with these provisions will be guilty of the offense of
+ * infringement and the penal sanctions provided for by law.
+ *
+ * © 2017 All rights reserved.
+ */
+
+namespace WBW\Library\SkiData\Tests\Parser;
+
+use DateTime;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SkiData\Entity\SkiDataStartRecordFormatEntity;
+use WBW\Library\SkiData\Parser\SkiDataStartRecordFormatParser;
+
+/**
+ * SkiData start record format parser test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SkiData\Tests\Parser
+ * @final
+ */
+final class SkiDataStartRecordFormatParserTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the parseEntity() method.
+	 *
+	 * @return void
+	 */
+	public function testParseEntity() {
+
+		$obj = new SkiDataStartRecordFormatEntity();
+		$obj->setVersionRecordStructure(190000);
+		$obj->setFacilityNumber(202747);
+		$obj->setDateFile(new DateTime("2017-09-21 16:10:00"));
+		$obj->setNumberRecords(18);
+		$obj->setCurrency("EUR");
+
+		$res = '190000;0202747;20170921;00018;"EUR"';
+		$this->assertEquals($res, (new SkiDataStartRecordFormatParser())->parseEntity($obj));
+	}
+
+	/**
+	 * Tests the parseLine() method.
+	 *
+	 * @return void
+	 */
+	public function testParseLine() {
+
+		$res = '190000;0202747;20170921;00018;"EUR"';
+
+		$obj = (new SkiDataStartRecordFormatParser())->parseLine($res);
+		$this->assertEquals(190000, $obj->getVersionRecordStructure());
+		$this->assertEquals(202747, $obj->getFacilityNumber());
+		$this->assertEquals(new DateTime("2017-09-21 00:00:00"), $obj->getDateFile());
+		$this->assertEquals(18, $obj->getNumberRecords());
+		$this->assertEquals("EUR", $obj->getCurrency());
+	}
+
+}
